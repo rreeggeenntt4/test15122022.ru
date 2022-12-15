e7ce09a5e762580ef0d51082f52d9083dfbd2a41
rreeggeenntt4 <114906878+rreeggeenntt4@users.noreply.github.com>
2022-12-15 12:36:21 +0300

    start test
    start test

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="icon" href="/favicon.ico">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/css/main.css" rel="stylesheet">
+  <title>Новости</title>
+</head>
+
+<body id="body">
+
+
+
+  <script src="/js/bootstrap.bundle.min.js"></script>
+  <script src="/js/main.js"></script>
+</body>
+
+</html>
