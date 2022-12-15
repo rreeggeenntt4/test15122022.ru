a6a6ad0cf7dc4c6cc4762a2f52e56a6a84849052
rreeggeenntt4 <114906878+rreeggeenntt4@users.noreply.github.com>
2022-12-15 16:08:42 +0300

    up
    up

--- index.php
+++ index.php
@@ -7,15 +7,47 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="/css/bootstrap.min.css" rel="stylesheet">
   <link href="/css/main.css" rel="stylesheet">
-  <title>Новости</title>
+  <title>Тест</title>
 </head>
 
 <body id="body">
-
-
-
   <script src="/js/bootstrap.bundle.min.js"></script>
   <script src="/js/main.js"></script>
+
+
+
+
+
+  <main class="col-10 col-sm-10 col-md-8 col-lg-6 col-xl-6 col-xxl-4 m-auto">
+    <form name="mainform" action="send_form.php" method="post" class="p-4 p-sm-5 border rounded-3 bg-light needs-validation" onsubmit="return(validate());">
+      <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="userName" name="userName" placeholder="Имя" required autocomplete="off">
+        <label for="userName">Имя</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="userEmail" name="userEmail" placeholder="name@example.com" required autocomplete="off">
+        <label for="userEmail">Email</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input type="password" class="form-control" id="Password" name="Password" placeholder="Password" minlength="6" required autocomplete="off">
+        <label for="Password">Password</label>
+      </div>
+      <div class="form-floating mb-5">
+        <input type="password" class="form-control" id="RePassword" name="RePassword" placeholder="RePassword" minlength="6" required autocomplete="off">
+        <label for="RePassword">RePassword</label>
+      </div>
+
+      <div class="text-center col-12 col-sm-12 col-md-10 col-lg-12 col-xl-12 col-xxl-10 m-auto">
+        <button hreff="" class="btn btn-lg btn-primary col-12 col-md-5 my-1" onclick="clearFilds();">Очистить</button>
+        <button class="btn btn-lg btn-primary col-12 col-md-5 my-1" type="submit">Отправить</button>
+      </div>
+
+      <hr class=" my-4">
+      <small class="text-muted mx-auto">Нажимая Отправить, вы соглашаетесь с условиями использования.</small>
+    </form>
+  </main>
+
+
 </body>
 
 </html>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+function validate() {
+    var username = document.getElementById("userName").value;
+    var email = document.getElementById("userEmail").value;
+    var password = document.getElementById("Password").value;
+    var repassword = document.getElementById("RePassword").value;
+    if (username.length <= 0) {
+        alert("Пожалуйста, введите имя пользователя.");
+        return false;
+    }
+    if (email.length <= 0) {
+        alert("Пожалуйста, введите адрес электронной почты.");
+        return false;
+    }
+    if ((password == null || password == "") && (password.length >= 6)) {
+        alert("Пожалуйста, введите пароль.");
+        return false;
+    }
+    if ((repassword == null || repassword == "" || repassword != password) && (password.length >= 6)) {
+        // Changed Password message for testing
+        alert("Пароль пуст или не соответствует.");
+        return false;
+    }
+}
 
+function clearFilds() {
+    /* console.log("Сработал"); */
+    document.getElementById("userName").value = "";
+    document.getElementById("userEmail").value = "";
+    document.getElementById("Password").value = "";
+    document.getElementById("RePassword").value = "";
+}
